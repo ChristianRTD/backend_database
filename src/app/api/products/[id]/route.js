2d3a376eb8c912a8524d28f6189101ae261829c0
ChristianRTD <98784405+ChristianRTD@users.noreply.github.com>
2025-02-02 22:54:06 -0500

    feat: :fire: function get

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,7 +1,44 @@
 import { NextResponse } from 'next/server';
+import { conn } from '@/libs/mysql';
 
-export  function GET() {
-  return NextResponse.json('OBTENIENDO UN PRODUCTO');
+export async function GET(request, { params }) {
+ 
+try {
+  
+  console.log(params.id);
+  const result = await conn.query('SELECT * FROM product WHERE id = ?', [params.id])
+  
+
+
+  if(result.lenght == 0){
+    return NextResponse.json({
+      message: "Producto no Encontrado",
+    },
+    {
+      status: 404,
+
+    }
+    );
+  }
+
+
+  return NextResponse.json(result[0]);
+
+
+} catch (error) {
+  
+return NextResponse.json(
+  {
+    message: error.message,
+
+  },
+  {
+    status: 500
+  }
+)
+
+}
+
 }
 
 export  function DELETE() {
