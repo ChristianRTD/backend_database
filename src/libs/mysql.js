b510fbe8ca0a0cc046e4b8da9635aa7641a7ada9
ChristianRTD <98784405+ChristianRTD@users.noreply.github.com>
2025-02-03 00:43:32 -0500

    feat: :zap:

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,9 +2,9 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config: {
-        host: 'sql-global.c5eoehvxjn2a.us-east-1.rds.amazonaws.com',
+        host: 'sql-global.c5eoehvxjn2a.us-east-1.rds.',
         user: 'Global',
-        password: 'globaltune202',
+        password: 'globaltune',
         port: 3306,
         database: 'users'
     }
